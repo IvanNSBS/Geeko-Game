19d90b683c974b9990295c58bdd5f4683525e07e
GuilleamACM <gacm@cin.ufpe.br>
2019-08-22 17:31:29 -0300

    Camera now has a pixel perfect component

---
+++ Geeko Project/Assets/Scripts/Utility Scripts/MapCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCamera : MonoBehaviour
+{
+    public GameObject mapRoot;
+    public GameObject mapCamera;
+    
+    void Start()
+    {
+        float posX = mapRoot.transform.position.x;
+        float posY = mapRoot.transform.position.y;
+        AdjustCameraPosition(posX, posY);
+    }
+
+    void AdjustCameraPosition(float x, float y)
+    {
+        mapCamera.transform.Translate(x, y, -10);
+    }
+}
