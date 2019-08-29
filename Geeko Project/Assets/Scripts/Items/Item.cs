6310a5a2797f10b53165721cc7266ca161e6fd27
IvanNSBS <insbs@cin.ufpe.br>
2019-08-29 19:21:31 -0300

    Adding Item script and Health Potion Scriptable Object

---
+++ Geeko Project/Assets/Scripts/Items/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class Item : ScriptableObject
+{
+    public string m_ItemName = "New Item";
+    public Sprite m_GameSprite;
+    public Animation m_Animation;
+    public AudioClip m_PickupSound;
+    public CollisionEvent m_OnPickup;
+
+    protected GameObject m_ItemPrefab;
+    protected ItemPrefabManager m_ItemManager;
+    public abstract void Initialize();
+    public abstract void PickupItem(GameObject target, GameObject src);
+    public void DestroyItem() { Destroy(this.m_ItemPrefab); }
+    public void SetPrefab(GameObject obj) { m_ItemPrefab = obj; }
+}
