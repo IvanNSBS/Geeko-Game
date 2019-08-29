72a6f9ea2dd56fc6a3713315dcfa54cf5573aaf6
GuilleamACM <gacm@cin.ufpe.br>
2019-08-29 15:51:59 -0300

    New UI Assets, health bars, joysticks. Player scaled reduced to 0.35

---
+++ Geeko Project/Assets/Scripts/Dungeon/DungeonManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class DungeonManager : MonoBehaviour
+{
+    List<Door> doors = new List<Door>();
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            doors = FindObjectsOfType<Door>().ToList();
+            foreach (Door d in doors)
+            {
+                d.OpenDoor();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            foreach(Door d in doors)
+            {
+                d.CloseDoor();
+            }
+        }
+    }
+}
