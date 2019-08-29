2dd5a0306e009631a515235d7eaf1038372c2edb
GuilleamACM <gacm@cin.ufpe.br>
2019-08-29 10:43:04 -0300

    LootTable System done

---
+++ Geeko Project/Assets/Scripts/Loot System/LootManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class Loot
+    {
+        public GameObject item;
+        public int dropRate;
+    }
+
+    public List<Loot> lootTable = new List<Loot>();
+    public int dropChance;
+    
+    void calculateLoot()
+    {
+        int m_dropChance = Random.Range(0, 101);
+        if(m_dropChance > dropChance)
+        {
+            Debug.Log("No Loot for me");
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            int itemWeight = 0;
+
+            for (int i = 0; i < lootTable.Count; i++)
+                itemWeight += lootTable[i].dropRate;
+
+            int randomValue = Random.Range(0, itemWeight);
+
+            for(int j = 0; j < lootTable.Count; j++)
+            {
+                if (randomValue <= lootTable[j].dropRate)
+                {
+                    Instantiate(lootTable[j].item, transform.position, Quaternion.identity);
+                    Destroy(this.gameObject);
+                    return;
+                }
+                randomValue -= lootTable[j].dropRate;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            calculateLoot();
+        }
+    }
+}
