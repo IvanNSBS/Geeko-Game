69fef239c8b4f93b8ee9a50c839901283981c091
GuilleamACM <gacm@cin.ufpe.br>
2019-09-14 20:54:50 -0300

    Pause button done and shoot button created

---
+++ Geeko Project/Assets/Scripts/Utility Scripts/PauseManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+
+    public bool GameIsPaused = false;
+    public GameObject PauseMenu;
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        PauseMenu.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        PauseMenu.SetActive(false);
+    }
+
+    public void OnApplicationPause(bool pause)
+    {
+        if (pause)
+        {
+            Pause();
+        }
+    }
+}
