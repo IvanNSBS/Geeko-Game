e7fe25d6657caee4b83ee7ea2869d85b97f78c63
Henrique Mariz <ham2@cin.ufpe.br>
2019-09-17 01:45:58 -0300

    Bat completed

---
+++ Geeko Project/Assets/Scripts/Actors/Enemies/BatController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatController : EnemyController
+{
+    [Header("Bat properties")]
+    public Animator batAnimator;
+    
+
+    public override void CheckTransitions()
+    {
+        if (IsPlayerInRange(sightRange) && currState != EnemyState.Die)
+        {
+            currState = EnemyState.Follow;
+        }
+
+        if (getIdle())
+        {
+            currState = EnemyState.Idle;
+        }
+        
+        if (GetCurrentHealth() <= 0)
+        {
+            currState = EnemyState.Die;
+        }
+        
+    }
+
+    
+    
+    
+    public void OnDeath()
+    {
+        batAnimator.SetBool("isDead",true);
+    }
+    
+    public override void StopMovement()
+    {
+        base.StopMovement();
+        batAnimator.SetBool("isMoving",false);
+    }
+    
+    public override void Idle()
+    {
+        base.Idle();
+        batAnimator.SetBool("isIdle",true);
+    }
+
+    public override void MoveEnemy(Vector3 dir, float speed)
+    {
+        base.MoveEnemy(dir, speed);
+        batAnimator.SetBool("isMoving", true);
+        batAnimator.SetBool("isIdle", false);
+    }
+
+    public override void OnCollisionEnter2D(Collision2D other)
+    {
+        base.OnCollisionEnter2D(other);
+        if (other.collider.CompareTag("Player"))
+        {
+            ResetFollowingTime();
+            SetTimeFollowing(0);
+        }
+    }
+}
