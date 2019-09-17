7aa0b4d7a58ff2bc123df975a9da7a9b6f4aac39
IvanNSBS <insbs@cin.ufpe.br>
2019-09-16 23:06:18 -0300

    Adding Rage Particle System

---
+++ Geeko Project/Assets/ParticleManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Range(0, 500)] float m_TimeToLive;
+
+    void Start()
+    {
+        if(m_TimeToLive > 0.0f)
+            Destroy(this.gameObject, m_TimeToLive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
