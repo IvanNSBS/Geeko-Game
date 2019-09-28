ba2194bfea7c84bacbfd28a7976b0df8dcf501f0
GuilleamACM <gacm@cin.ufpe.br>
2019-09-28 17:32:54 -0300

    GameOver overlay done

---
+++ Geeko Project/Assets/Gameover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameover : MonoBehaviour
+{
+    private void DestroyPlayerAndDungeonManager()
+    {
+        Destroy(GameObject.FindGameObjectWithTag("Player"));
+        Destroy(FindObjectOfType<DungeonManager>().gameObject);
+    }
+
+    public void Retry()
+    {
+        FindObjectOfType<LoadTargetScene>().LoadSceneNum(2);
+        Time.timeScale = 1f;
+        DestroyPlayerAndDungeonManager();
+    }
+
+    public void BackToMainMenu()
+    {
+        FindObjectOfType<LoadTargetScene>().LoadSceneNum(0);
+        Time.timeScale = 1f;
+        DestroyPlayerAndDungeonManager();
+    }
+}
