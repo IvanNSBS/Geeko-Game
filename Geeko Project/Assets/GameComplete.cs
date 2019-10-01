ee81907dbb680003ff2ba5cd395e429d0ecd2237
GuilleamACM <gacm@cin.ufpe.br>
2019-10-01 05:57:47 -0300

    GameComplete script updated to activate the playerprogress

---
+++ Geeko Project/Assets/GameComplete.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameComplete : MonoBehaviour
+{
+    public GameObject Progress;
+  
+    public void SetGameCompleteActive(bool b)
+    {
+        Progress.SetActive(b);
+        this.gameObject.SetActive(b);
+    }
+}
