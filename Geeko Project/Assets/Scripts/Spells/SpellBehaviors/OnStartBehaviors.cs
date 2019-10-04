9c930d0345a0ff2905d720de854f4dbd23357a3d
IvanNSBS <insbs@cin.ufpe.br>
2019-10-04 10:56:55 -0300

    Creating OnStartBehavior.cs and moving stuff to there

---
+++ Geeko Project/Assets/Scripts/Spells/SpellBehaviors/OnStartBehaviors.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class OnStartData
+{
+    public FollowRuleData m_FollowRuleData;
+    public OnStartBehavior m_StartBehavior;
+}
+
+[Serializable]
+public class FollowRuleData
+{
+    public FollowWho m_FollowRule = FollowWho.Player;
+    public Vector3 m_FollowOffset;
+    public float m_FollowSmoothDamp = 0.1f;
+    public bool m_UseAimedTarget = false;
+}
+
+[System.Serializable]
+public abstract class OnStartBehavior : ScriptableObject
+{
+    public abstract void OnStartEvent(GameObject src, OnStartData data);
+}
+
+[CreateAssetMenu(menuName = "SpellBehavior/Set Follow Rule")]
+public class SetFollowRule : OnStartBehavior
+{
+    public override void OnStartEvent(GameObject src, OnStartData data)
+    {
+        src.GetComponent<SpellPrefabManager>().SetFollowerOwner(data.m_FollowRuleData.m_FollowRule);
+        src.GetComponent<SpellPrefabManager>().m_FollowOffset = data.m_FollowRuleData.m_FollowOffset;
+        src.GetComponent<SpellPrefabManager>().m_FollowSmoothDamp = data.m_FollowRuleData.m_FollowSmoothDamp;
+        src.GetComponent<SpellPrefabManager>().SetUseAimedTarget(data.m_FollowRuleData.m_UseAimedTarget);
+    }
+}
--- Geeko Project/Assets/Scripts/Spells/SpellBehaviors/SpellBehaviors.cs
+++ Geeko Project/Assets/Scripts/Spells/SpellBehaviors/SpellBehaviors.cs
@@ -20,16 +20,6 @@ public class OnTickData
 }
 
 [Serializable]
-public class OnStartData
-{
-    public FollowWho m_FollowRule = FollowWho.Player;
-    public Vector3 m_FollowOffset;
-    public float m_FollowSmoothDamp = 0.1f;
-    public bool m_UseAimedTarget = false;
-    public OnStartBehavior m_StartBehavior;
-}
-
-[Serializable]
 public class OnDestructionData
 {
 }
@@ -55,30 +45,9 @@ public abstract class OnTickBehavior : ScriptableObject
 }
 
 [System.Serializable]
-public abstract class OnStartBehavior : ScriptableObject
-{
-    public abstract void OnStartEvent(GameObject src, OnStartData data);
-}
-
-[System.Serializable]
 public abstract class OnDestructionBehavior : ScriptableObject
 {
     public abstract void OnDestructionEvent(OnDestructionData data);
-}
-
-
-
-
-[CreateAssetMenu(menuName = "SpellBehavior/Set Follow Rule")]
-public class SetFollowRule : OnStartBehavior
-{
-    public override void OnStartEvent(GameObject src, OnStartData data)
-    {
-        src.GetComponent<SpellPrefabManager>().SetFollowerOwner(data.m_FollowRule);
-        src.GetComponent<SpellPrefabManager>().m_FollowOffset = data.m_FollowOffset;
-        src.GetComponent<SpellPrefabManager>().m_FollowSmoothDamp = data.m_FollowSmoothDamp;
-        src.GetComponent<SpellPrefabManager>().SetUseAimedTarget(data.m_UseAimedTarget);
-    }
 }
 
 
