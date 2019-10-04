45fe2d3a4c99cd9bb9876b6ab7b3c3434bdd1307
IvanNSBS <insbs@cin.ufpe.br>
2019-10-04 17:42:29 -0300

    Updating ManaShield to partially use new system

--- Geeko Project/Assets/Scripts/Spells/ScriptableObjects/ManaShieldConc.cs
+++ Geeko Project/Assets/Scripts/Spells/ScriptableObjects/ManaShieldConc.cs
@@ -10,6 +10,7 @@ public class ManaShieldConc : Spell
     [SerializeField] private float m_Radius = 3.0f;
     [SerializeField] private float m_IFrameTime = 1.0f;
     [SerializeField] private Material m_Material;
+    [SerializeField] private List<OnStartData> m_OnStartEvents;
     public override GameObject CastSpell(GameObject owner, GameObject target = null, Vector3? spawn_pos = null, Quaternion? spawn_rot = null)
     {
         if (m_Prefab && owner)
@@ -24,10 +25,17 @@ public class ManaShieldConc : Spell
             obj.GetComponent<CircleCollider2D>().radius = 0.5f;
             obj.GetComponent<CircleCollider2D>().isTrigger = true;
             obj.transform.localScale *= (2*m_Radius);
-            CircleCollider2D circle = obj.GetComponent<SpellPrefabManager>().GetOwner().GetComponent<CircleCollider2D>();
+            Collider2D circle = obj.GetComponent<SpellPrefabManager>().GetOwner().GetComponent<Collider2D>();
             Vector3 pos = circle.bounds.center + new Vector3(0, 0, -15);
-            obj.transform.position = pos;
+            //obj.transform.position = pos;
             // GameplayStatics.AddQuad(obj, m_Material);
+
+            //obj.GetComponent<SpellPrefabManager>().SetFollowerOwner(FollowWho.Player);
+            //obj.GetComponent<SpellPrefabManager>().m_FollowOffset = new Vector3(0, 0, -15) + circle.bounds.center - obj.GetComponent<SpellPrefabManager>().GetOwner().transform.position;
+            //obj.GetComponent<SpellPrefabManager>().m_FollowSmoothDamp = 0.0f;
+
+            foreach (var startevent in m_OnStartEvents)
+                startevent.m_StartBehavior.OnStartEvent(obj, startevent);
 
             if (m_Material)
                 obj.GetComponent<MeshRenderer>().material = m_Material;
@@ -56,9 +64,9 @@ public class ManaShieldConc : Spell
 
     public override void OnTick(GameObject obj)
     {
-        CircleCollider2D circle = obj.GetComponent<SpellPrefabManager>().GetOwner().GetComponent<CircleCollider2D>();
-        Vector3 pos = circle.bounds.center + new Vector3(0,0,-15);
-        obj.transform.position = pos;
+        //CircleCollider2D circle = obj.GetComponent<SpellPrefabManager>().GetOwner().GetComponent<CircleCollider2D>();
+        //Vector3 pos = circle.bounds.center + new Vector3(0,0,-15);
+        //obj.transform.position = pos;
         SpellUtilities.UpdateSpellTTL(obj, this);
     }
 
--- Geeko Project/Assets/Scripts/Spells/SpellBehaviors/OnStartBehaviors.cs
+++ Geeko Project/Assets/Scripts/Spells/SpellBehaviors/OnStartBehaviors.cs
@@ -14,7 +14,9 @@ public class FollowRuleData
     public FollowWho m_FollowRule = FollowWho.Player;
     public Vector3 m_FollowOffset;
     public float m_FollowSmoothDamp = 0.1f;
+    public bool m_CenterAroundCaster = false;
     public bool m_UseAimedTarget = false;
+    public bool m_MoveImmediately = false;
 }
 
 [System.Serializable]
@@ -28,9 +30,25 @@ public class SetFollowRule : OnStartBehavior
 {
     public override void OnStartEvent(GameObject src, OnStartData data)
     {
-        src.GetComponent<SpellPrefabManager>().SetFollowerOwner(data.m_FollowRuleData.m_FollowRule);
-        src.GetComponent<SpellPrefabManager>().m_FollowOffset = data.m_FollowRuleData.m_FollowOffset;
-        src.GetComponent<SpellPrefabManager>().m_FollowSmoothDamp = data.m_FollowRuleData.m_FollowSmoothDamp;
-        src.GetComponent<SpellPrefabManager>().SetUseAimedTarget(data.m_FollowRuleData.m_UseAimedTarget);
+        var manager = src.GetComponent<SpellPrefabManager>();
+        var owner = manager.GetOwner();
+        if (data.m_FollowRuleData.m_CenterAroundCaster)
+        {
+            var circle = owner.GetComponent<Collider2D>();
+            if(data.m_FollowRuleData.m_MoveImmediately)
+                src.transform.position = data.m_FollowRuleData.m_FollowOffset + circle.bounds.center - owner.transform.position;
+            manager.m_FollowOffset = data.m_FollowRuleData.m_FollowOffset + circle.bounds.center - owner.transform.position;
+        }
+        else
+        {
+            var circle = owner.GetComponent<Collider2D>();
+            if (data.m_FollowRuleData.m_MoveImmediately)
+                src.transform.position = data.m_FollowRuleData.m_FollowOffset + circle.bounds.center - owner.transform.position;
+            manager.m_FollowOffset = data.m_FollowRuleData.m_FollowOffset;
+        }
+
+        manager.SetFollowerOwner(data.m_FollowRuleData.m_FollowRule);
+        manager.m_FollowSmoothDamp = data.m_FollowRuleData.m_FollowSmoothDamp;
+        manager.SetUseAimedTarget(data.m_FollowRuleData.m_UseAimedTarget);
     }
 }
